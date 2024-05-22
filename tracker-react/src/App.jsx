0b8e9e7d692969fca0bc9296f84a73bdc730cbb6
“bassceo” <meregodz@gmail.com>
2024-05-23 02:49:46 +0300

    mini update

--- tracker-react/src/App.jsx
+++ tracker-react/src/App.jsx
@@ -3,13 +3,13 @@ import Accounts from "./components/Accounts";
 import {Routes, Route} from "react-router-dom";
 import './index.css';
 import Transactions from "./components/Transactions";
-import HomePage from "./components/HomePage";
+// import HomePage from "./components/HomePage";
 
 const App = () => {
   return < >
     <NavBar/>
     <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<Accounts/>} />
       <Route path="/accounts" element={<Accounts/>}/>
       <Route path="/transactions" element={<Transactions/>}/>
 
